de98422431c1bf0cac2676ab1c19c910f3186ed3
Vinicyus Silva <vinicyus346@gmail.com>
2024-02-12 21:23:22 -0300

    feat: adding find all companies route

--- src/4-framework/controllers/company/company.ts
+++ src/4-framework/controllers/company/company.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { FindByCompanyController } from './findBy'
+import { FindAllCompaniesController } from './findAll'
 
 const companyRoutes = Router()
 
 const findByCompany = new FindByCompanyController()
+const findAllCompanies = new FindAllCompaniesController()
 
 companyRoutes.get('/:id', findByCompany.handle)
+companyRoutes.get('/', findAllCompanies.handle)
 
 export { companyRoutes }
