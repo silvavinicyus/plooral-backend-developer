81a80c2deb30b5be10c8ea117110337236c41ceb
Vinicyus Silva <vinicyus346@gmail.com>
2024-02-16 19:55:37 -0300

    feat: get feed controller

---
+++ src/4-framework/controllers/job/feed.ts
@@ -0,0 +1,26 @@
+import '@framework/ioc/inversify.config'
+import { GetFeedJobsUseCase } from '@root/src/3-useCases/operations/job/feed'
+import { IError } from '@shared/IError'
+import { container } from '@shared/ioc/container'
+import { Request, Response } from 'express'
+
+export class GetFeedJobController {
+  async handle(_request: Request, response: Response): Promise<Response> {
+    try {
+      const useCase = container.get(GetFeedJobsUseCase)
+      const feedResult = await useCase.run()
+
+      if (feedResult.isLeft()) throw feedResult.value
+
+      return response.status(200).json(feedResult.value)
+    } catch (err) {
+      console.error(err)
+
+      if (err instanceof IError) {
+        return response.status(err.statusCode).json(err.body)
+      }
+
+      return response.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+}
