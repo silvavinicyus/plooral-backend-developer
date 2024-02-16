fdb147a2176aee6533d21b5cb6c83187b48efa59
Vinicyus Silva <vinicyus346@gmail.com>
2024-02-16 19:56:30 -0300

    feat: new job routes

--- src/4-framework/controllers/job/job.ts
+++ src/4-framework/controllers/job/job.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { CreateJobController } from './create'
-import { PublishJobController } from './publish'
 import { ArchiveJobController } from './archive'
+import { CreateJobController } from './create'
 import { DeleteJobController } from './delete'
+import { GetFeedJobController } from './feed'
+import { PublishJobController } from './publish'
 import { UpdateJobController } from './update'
 
 const jobRoutes = Router()
@@ -12,11 +13,13 @@ const publishJob = new PublishJobController()
 const archiveJob = new ArchiveJobController()
 const deleteJob = new DeleteJobController()
 const updateJob = new UpdateJobController()
+const feedJob = new GetFeedJobController()
 
 jobRoutes.post('/', createJob.handle)
 jobRoutes.put('/:job_id/publish', publishJob.handle)
 jobRoutes.put('/:job_id/archive', archiveJob.handle)
 jobRoutes.delete('/:job_id', deleteJob.handle)
 jobRoutes.put('/:job_id', updateJob.handle)
+jobRoutes.get('/feed', feedJob.handle)
 
 export { jobRoutes }
