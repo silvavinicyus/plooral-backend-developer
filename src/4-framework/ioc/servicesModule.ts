0101fe6d6968926acf14c63dd39432e46e1f0ee0
Vinicyus Silva <vinicyus346@gmail.com>
2024-02-16 19:53:30 -0300

    feat: ioc imports for new external services

--- src/4-framework/ioc/servicesModule.ts
+++ src/4-framework/ioc/servicesModule.ts
@@ -1,5 +1,16 @@
+import {
+  IQueueService,
+  IQueueServiceToken,
+} from '@business/extServices/queue/iQueueService'
+import {
+  IStorageService,
+  IStorageServiceToken,
+} from '@business/extServices/storage/iStorageService'
+import { SqsService } from '@framework/extService/queue/SqsService'
+import { S3StorageService } from '@framework/extService/storage/S3Storage'
 import { ContainerModule, interfaces } from 'inversify'
 
-export const servicesModule = new ContainerModule(
-  (_bind: interfaces.Bind) => {}
-)
+export const servicesModule = new ContainerModule((bind: interfaces.Bind) => {
+  bind<IQueueService>(IQueueServiceToken).to(SqsService)
+  bind<IStorageService>(IStorageServiceToken).to(S3StorageService)
+})
